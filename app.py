1a63a6165f3ab8b88944ce16be62091af23936a1
paritoshk <pck2123@columbia.edu>
2023-01-10 20:48:40 -0800

    git commit -m "AN-21 <ongoing>"

--- app.py
+++ app.py
@@ -4,24 +4,101 @@ import pandas as pd
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 import numpy as np
+import regex as re
+import random
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:30px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+@st.cache(allow_output_mutation=True)
+def find_indexes_of_matching_keywords(list_of_keywords,df,column):
+    """" function that takes in a input list of strings, a dataframe and a column containing 
+    lists as input and returns indexes of rows that match elements in the input list with 
+    elements in lists in the column"""
+    """match a string.
+    :parameter
+        :param column: string - name of column containing lists of text to match
+        :param list_of_keywords: list - list of keywords to match
+        :param df: dataframe - dataframe containing column
+    :return
+        index list"""
+
+    indexes = []
+    matched_words = []
+    df_copy = df.copy(deep=True)
+    df_copy[column] = df_copy[column].apply(lambda x: [item.lower() for item in x])
+    list_of_keywords = [item.lower() for item in list_of_keywords]
+    for i in range(len(df_copy[column])):
+        for p in list_of_keywords:
+            for j in range(len(df_copy[column][i])):
+                    if p in df[column][i][j]:
+                         indexes.append(i)
+                         matched_words.append((p,df[column][i][j]))
+    return indexes, matched_words
 
 
-@st.cache
+@st.cache(allow_output_mutation=True)
+def display_dataframe_withindex(df,indexes):
+    """Display dataframe."""
+    df_display = df.iloc[indexes]
+    return df_display
+
+@st.cache(allow_output_mutation=True)
+def random_list(list,n):
+    """ function that creates a random list of n elements from a long list"""
+    random_list = []
+    for i in range(n):
+        random_list.append(random.choice(list))
+    return random_list
+
+@st.cache(allow_output_mutation=True)
+def combine_list_column(df,column):
+    """ function that combines a column of lists in dataframe to a large list """
+    df_copy = df.copy(deep=True)
+    list_of_lists = df_copy[column].tolist()
+    list_of_lists = [item for sublist in list_of_lists for item in sublist]
+    return list(set(list_of_lists)) #drops duplicates
+
+
+@st.cache(allow_output_mutation=True)
+def convert_list_of_strings_to_list(list_words:list)->list:
+    """ function that converts a list of strings sperated by comma into a list each strings"""
+    list_output = []
+    for text in list_words:
+        text = text.split(',')
+        for word in text:
+            word = word.strip()
+            list_output.append(word)
+    return list_output
+
+
+
+#usage of above functions examples 
+#list_combined_keywords = combine_list_column(df_final_abstracts_clean,'keywords')
+#list_combined_keywords_final =(convert_list_of_strings_to_list(list_combined_keywords))
+#find_indexes_of_matching_keywords(test_list[2:4],df_final_abstracts_clean,'keywords')
+#display_dataframe_withindex(df_final_abstracts_clean,find_indexes_of_matching_keywords(test_list,df_final_abstracts_clean,'keywords')[0])
+
+@st.cache(allow_output_mutation=True)
+def display_dataframe(df):
+    titile = df.iloc[0].title
+    return st.markdown('<p class="big-font">{0}</p>'.format(titile), unsafe_allow_html=True)
+
+
+@st.cache(allow_output_mutation=True)
 def read_data(data="data/publications/post2018_agingcomapnies_papers.csv"):
-    """Read the data from local."""
-    data = pd.read_csv(data)
-    # datetime conversation for display
-    data['publication_date'] = pd.to_datetime(data['publication_date'])
-    data['publication_date'] = data['publication_date'].dt.date
-    # to capitalize each row in the company_name column.
-    data['company_name'] = data['company_name'].str.capitalize()
     return data
 
 
 @st.cache(allow_output_mutation=True)
 def load_bert_model():
     """Instantiate a sentence-level allenai-specer model."""
-    return SentenceTransformer('sentence-transformers/allenai-specter')
+    return SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 @st.cache(allow_output_mutation=True)
 def load_embeddings(path_to_embeddings="data/models/embeddings_allabstracts.pickle"):
@@ -40,9 +117,8 @@ def load_faiss_index(path_to_faiss="data/models/faiss_index_allabstracts.pickle"
 
 @st.cache(allow_output_mutation=True)
 def vector_search(query, model, index, num_results=10):
-    """Tranforms query to vector using #https://huggingface.co/sentence-transformers/allenai-specter
-model = SentenceTransformer('sentence-transformers/allenai-specter')
-and finds similar vectors using FAISS.
+    """Tranforms query to vector using #https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+    and finds similar vectors using FAISS.
     
     Args:
         query (str): User query that should be more than a sentence long.
@@ -64,9 +140,6 @@ def id2details(df, I, column):
     """Returns the paper titles based on the paper index."""
     return [list(df[df.id == idx][column]) for idx in I[0]]
 
-
-
-
 def main():
     try:
     
@@ -78,14 +151,18 @@ def main():
         # important columns - company_name, article_id, title, keywords, publication_date, abstract, journal, doi, authors
         # variables - user_input, filter_company, num_results
         comapny_list = list(set(data['company_name'].tolist()))
+        list_combined_keywords = combine_list_column(data,'keywords')
+        list_combined_keywords_final =(convert_list_of_strings_to_list(list_combined_keywords))
 
-        st.title("Vectorized Search over 🧬 PubMed abstracts database curated within :blue[anti-aging] industry and research👨‍🔬⚗️🧪💊*")
+        st.title("🧬 Semantic Search over PubMed abstracts curated within :blue[anti-aging] industry and research👨‍🔬⚗️🧪💊*")
         st.caption("""There are over _3000_ abstracts in the database taken from PubMed. All publications are from _2018_ onwards.
-                   These are compiled from 100 operating companies in the :blue[anti-aging] space, inlcuding Altos Labs, Unity Biotechnology, Insilico Medicine, and many more.""")
+                   These are compiled from 97 operating companies in the :blue[anti-aging] space, inlcuding Altos Labs, Unity Biotechnology, Insilico Medicine, and many more.""")
         st.caption("""The data about the companies is taken from [this](https://agingbiotech.info/companies) maintained by investor and creator Karl Pfleger.""")
 
         # User search
         user_input = st.text_area("Search box-Experimental", "stem cell research")
+        # Keyword search
+        keyword_list = st.multi_select('Select Keywords (Preffered)',list_combined_keywords, 'stem cell') #get dropdown of keywords
 
         # Filters
         st.sidebar.markdown("**Filters**")
@@ -113,12 +190,18 @@ def main():
                 else:
                     continue
                 newline= '\n'
+                display_dataframe(f)
+                st.markdown(f"**Keywords**:")
+                for i in ((f.iloc[0].keywords).strip('"')).split(','):
+                    # clean string from all non alphanumeric characters
+                    i = re.sub(r'\W+', '', i)
+                    st.markdown(f"{i}")
+                
                 st.write(
-                        f"""**{f.iloc[0].title}**  
-                    **Affiliate Anti-Aging Company Name**: {f.iloc[0].company_name.capitalize()}
-                    **Journal**: {f.iloc[0].journal}  
-                    **Publication Date**: {f.iloc[0].publication_date}  
-                    {newline}**Keywords**: *{f.iloc[0].keywords}*
+                        f"""
+                    {newline}**Affiliate Anti-Aging Company Name**: {f.iloc[0].company_name.capitalize()}
+                    {newline}**Journal**: {f.iloc[0].journal}  
+                    {newline}**Publication Date**: {f.iloc[0].publication_date}  
                     {newline}**DOI**: *{f.iloc[0].doi.split(newline)[0]}*
                     {newline}**Abstract**: {f.iloc[0].abstract}
                     """
@@ -134,13 +217,19 @@ def main():
                     f = frame[(frame.article_id == id_)]
 
                     newline= '\n'
+                    display_dataframe(f)
+                    st.markdown(f"**Keywords**:")
+                    keywords = []
+                    for i in ((f.iloc[0].keywords).strip('"')).split(','):
+                        i = re.sub(r'\W+', '', i)
+                        keywords.append(i)
 
                     st.write(
-                        f"""**{f.iloc[0].title}**  
-                    **Affiliate Anti-Aging Company Name**: {f.iloc[0].company_name.capitalize()}
-                    **Journal**: {f.iloc[0].journal}  
-                    **Publication Date**: {f.iloc[0].publication_date}  
-                    {newline}**Keywords**: *{f.iloc[0].keywords}*
+                        f"""
+                    {newline}**Affiliate Anti-Aging Company Name**: {f.iloc[0].company_name.capitalize()}
+                    {newline}**Journal**: {f.iloc[0].journal}  
+                    {newline}**Publication Date**: {f.iloc[0].publication_date}  
+                    {newline}**Keywords**: *{keywords}*
                     {newline}**DOI**: *{f.iloc[0].doi.split(newline)[0]}*
                     {newline}**Abstract**: {f.iloc[0].abstract}
                     """
